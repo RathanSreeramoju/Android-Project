62ea63262e214b0cad204f41867bcc7bd375b47b
tinkuaravind <52049907+tinkuaravind@users.noreply.github.com>
2020-02-19 14:39:20 -0500

    Maths Java files

---
+++ app/src/main/java/com/example/quiz/maths2question.java
@@ -0,0 +1,14 @@
+package com.example.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class maths2question extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maths2question);
+    }
+}
